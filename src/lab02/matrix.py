fd83ba81151860a77ce90e23510a943392d519ee
DarkThistle <ekor32642@gmail.com>
2026-09-17 11:02:23 +0300

    new file:   images/lab02/col_sums_1.png 	new file:   images/lab02/col_sums_2.png 	new file:   images/lab02/col_sums_3.png 	new file:   images/lab02/col_sums_4.png 	new file:   images/lab02/min_max_1.png 	new file:   images/lab02/min_max_2.png 	new file:   images/lab02/min_max_3.png 	new file:   images/lab02/min_max_4.png 	new file:   images/lab02/min_max_5.png 	new file:   images/lab02/row_sums_1.png 	new file:   images/lab02/row_sums_2.png 	new file:   images/lab02/row_sums_3.png 	new file:   images/lab02/row_sums_4.png 	new file:   images/lab02/transpose_1.png 	new file:   images/lab02/transpose_2.png 	new file:   images/lab02/transpose_3.png 	new file:   images/lab02/transpose_4.png 	new file:   images/lab02/transpose_5.png 	deleted:    python_labs 	modified:   src/lab02/arrays.py 	new file:   src/lab02/matrix.py

---
+++ src/lab02/matrix.py
@@ -0,0 +1,25 @@
+def transpose(mat: list[list[float | int]]) -> list[list]:
+    trans = []
+    if len(mat) == 0:
+        return []
+    if len(set(map(lambda x: len(x), mat))) > 1:
+        raise ValueError
+    return [[mat[j][i] for j in range(len(mat))]for i in range(len(mat[0]))]
+
+#print(f"Ввод: {transpose(eval(input("Ввод: ")))}")
+
+
+def row_sums(mat: list[list[float | int]]) -> list[float]:
+    if len(set(map(lambda x: len(x), mat))) > 1:
+            raise ValueError
+    return list(map(lambda x: sum(x), mat))
+
+#print(f"Ввод: {row_sums(eval(input("Ввод: ")))}")
+
+
+def col_sums(mat: list[list[float | int]]) -> list[float]:
+    if len(set(map(lambda x: len(x), mat))) > 1:
+                raise ValueError
+    return [sum([mat[j][i] for j in range(len(mat))]) for i in range(len(mat[0]))]
+
+#print(f"Ввод: {col_sums(eval(input("Ввод: ")))}")
