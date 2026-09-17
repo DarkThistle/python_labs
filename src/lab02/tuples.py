892899ad859ce4e84132bca5c3fc2604f2f6ec98
DarkThistle <ekor32642@gmail.com>
2026-09-17 12:01:04 +0300

    modified:   src/lab02/tuples.py

--- src/lab02/tuples.py
+++ src/lab02/tuples.py
@@ -1,4 +1,10 @@
 def format_record(rec: tuple[str, str, float]) -> str:
+    if rec[0] == "" or rec[1] == "":
+        raise ValueError
+    if type(rec[2]) != float:
+        raise TypeError
     name = [i.capitalize() for i in rec[0].split()]
     return f"{name[0]} {" ".join([i[0]+"." for i in name[1:]])}, гр. {rec[1]}, GPA {rec[2]:.02f}"
+
 print(format_record(eval(input())))
+
