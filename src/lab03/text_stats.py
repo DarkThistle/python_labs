3a8eda45909b009437f1d20dfc265ac9de6e7022
DarkThistle <ekor32642@gmail.com>
2026-09-23 13:03:00 +0300

    Remove __pycache__ from tracking and update .gitignore

--- src/lab03/text_stats.py
+++ src/lab03/text_stats.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from lib.text import normalize, tokenize, count_freq, top_n
 
 best_input_in_our_universe = normalize(input())
