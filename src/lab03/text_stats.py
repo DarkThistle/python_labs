87a8aa743782add57c5a1c1df40a24505b77e158
DarkThistle <ekor32642@gmail.com>
2026-09-23 21:54:23 +0300

    new file:   .gitignore 	modified:   src/lab03/text_stats.py

--- src/lab03/text_stats.py
+++ src/lab03/text_stats.py
@@ -5,16 +5,16 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from lib.text import normalize, tokenize, count_freq, top_n
 
-best_input_in_our_universe = normalize(input())
-table_switcher = input().strip() == "True"
-words_num = len(tokenize(best_input_in_our_universe))
-unique_num = len(count_freq(tokenize(best_input_in_our_universe)))
-top_5 = top_n(count_freq(tokenize(best_input_in_our_universe)))
-if not table_switcher:
-    print(
-        f"Всего слов: {words_num}",
-        f"Уникальных слов: {unique_num}",
-        f"Топ-5: \n{"\n".join([":".join([i[0], str(i[1])]) for i in top_5])}",
-        sep="\n"
-        )
-    
+def text_stata(textik, flag=False):
+
+    norm_text = normalize(textik)
+    token_text = tokenize(norm_text)
+    word_freq = count_freq(token_text)
+
+    if flag == False:
+
+        words_num = len(token_text)
+        unique_num = len(word_freq)
+        top_5 = top_n(word_freq)
+
+        return f"Всего слов: {words_num}\nУникальных слов: {unique_num}\nТоп-5: \n{"\n".join([":".join([i[0], str(i[1])]) for i in top_5])}"
