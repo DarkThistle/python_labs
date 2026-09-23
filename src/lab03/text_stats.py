4b53be9cd03c49dd5e29d77b6d248640c20ede8e
DarkThistle <ekor32642@gmail.com>
2026-09-23 10:34:22 +0300

    modified:   src/lab03/text_stats.py 	new file:   src/lib/__pycache__/text.cpython-314.pyc

--- src/lab03/text_stats.py
+++ src/lab03/text_stats.py
@@ -1,4 +1,10 @@
-from src.lib.text import normalize, tokenize, count_freq, top_n
+import sys
+import os
+
+# Получаем абсолютный путь к папке src (на уровень выше lab03)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from lib.text import normalize, tokenize, count_freq, top_n
 
 best_input_in_our_universe = normalize(input())
 table_switcher = input().strip() == "True"
@@ -9,7 +15,7 @@ if not table_switcher:
     print(
         f"Всего слов: {words_num}",
         f"Уникальных слов: {unique_num}",
-        f"Топ-5: \n{"\n".join([":".join(i) for i in top_5])}",
+        f"Топ-5: \n{"\n".join([":".join([i[0], str(i[1])]) for i in top_5])}",
         sep="\n"
         )
     
